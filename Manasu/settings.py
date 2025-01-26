0a5eef4a8143bbae32a70318ede9761f874466d1
Roshan Paul Babu <89399674+RoshanPaulBabu@users.noreply.github.com>
2025-01-26 18:06:53 +0530

    Update settings.py

--- Manasu/settings.py
+++ Manasu/settings.py
@@ -78,28 +78,17 @@ WSGI_APPLICATION = 'Manasu.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'manasudatabase',
-        'USER': 'roshan',
-        'PASSWORD':'ChristeenaBabu',
-        'HOST': 'database-1.c7a82ussk6qs.eu-north-1.rds.amazonaws.com',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 
 # Password validation
